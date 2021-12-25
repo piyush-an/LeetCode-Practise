62fa4a06eed175a6b7d995194195cbe9f0f860e6
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-24 21:48:53 -0500

    Time: 1407 ms (63.32%), Space: 0B (100.00%) - LeetHub

---
+++ classes-more-than-5-students/classes-more-than-5-students.sql
@@ -0,0 +1,22 @@
+-- Schema
+-- DROP TABLE IF EXISTS Courses
+-- Create table Courses (student varchar(255), class varchar(255))
+-- Truncate table Courses
+-- insert into Courses (student, class) values ('A', 'Math')
+-- insert into Courses (student, class) values ('B', 'English')
+-- insert into Courses (student, class) values ('C', 'Math')
+-- insert into Courses (student, class) values ('D', 'Biology')
+-- insert into Courses (student, class) values ('E', 'Math')
+-- insert into Courses (student, class) values ('F', 'Computer')
+-- insert into Courses (student, class) values ('G', 'Math')
+-- insert into Courses (student, class) values ('H', 'Math')
+-- insert into Courses (student, class) values ('I', 'Math')
+
+-- Solution 1
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) > 4
+
+-- End
