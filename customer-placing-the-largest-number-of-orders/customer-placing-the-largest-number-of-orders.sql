433c852baebfc74c462df13994c82a2d83dd18cb
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-24 21:17:34 -0500

    Time: 1010 ms (84.17%), Space: 0B (100.00%) - LeetHub

---
+++ customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,19 @@
+-- Schema
+-- Create table orders (order_number int, customer_number int)
+-- Truncate table orders
+-- insert into orders (order_number, customer_number) values (1, 1)
+-- insert into orders (order_number, customer_number) values (2, 2)
+-- insert into orders (order_number, customer_number) values (3, 3)
+-- insert into orders (order_number, customer_number) values (4, 3)
+
+-- Solution
+
+SELECT customer_number
+FROM (SELECT TOP 1
+        customer_number, count(order_number) AS total_order
+    FROM orders
+    GROUP BY customer_number
+    ORDER BY total_order DESC) result
+
+
+-- End
