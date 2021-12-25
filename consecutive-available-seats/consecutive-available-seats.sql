c7e81d593f7052cc278a1ce0bfeb69b03d40317e
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-25 00:51:23 -0500

    Time: 1090 ms (41.93%), Space: 0B (100.00%) - LeetHub

--- consecutive-available-seats/consecutive-available-seats.sql
+++ consecutive-available-seats/consecutive-available-seats.sql
@@ -11,6 +11,7 @@
 -- Solution 1
 SELECT c1.seat_id
 FROM Cinema c1 LEFT JOIN Cinema c2 ON c2.seat_id = (c1.seat_id + 1) LEFT JOIN Cinema c3 ON c3.seat_id = (c1.seat_id - 1)
-WHERE c1.free = 1 AND (c2.free = 1 OR c3.free = 1)
+WHERE c1.free = 1 AND (c2.free = 1 OR c3.free = 1) 
+ORDER BY c1.seat_id
 
 -- End
