21ad687ef7aedead79a2c65a45d582f6bf9ab9b8
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-25 01:54:22 -0500

    Time: 1779 ms (18.66%), Space: 0B (100.00%) - LeetHub

---
+++ shortest-distance-in-a-line/shortest-distance-in-a-line.sql
@@ -0,0 +1,19 @@
+
+-- SCHEMA
+-- DROP TABLE IF EXISTS Point
+-- Create Table Point (x int not null)
+-- Truncate table Point
+-- insert into Point (x) values (-1)
+-- insert into Point (x) values (0)
+-- insert into Point (x) values (2)
+
+-- SOLUTION
+
+SELECT TOP 1
+    -- p1.x as PointA, p2.x as PointB, 
+    ABS(p1.x - p2.x) AS shortest
+FROM Point p1 CROSS JOIN Point p2
+where p1.x != p2.x
+ORDER BY shortest
+
+-- END
