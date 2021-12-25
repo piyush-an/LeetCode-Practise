8552e62f4aa00d9df646bfcbfa5417e14e7b783c
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-25 00:07:02 -0500

    Time: 1030 ms (52.18%), Space: 0B (100.00%) - LeetHub

---
+++ friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -0,0 +1,49 @@
+-- Schema
+-- DROP TABLE IF EXISTS FriendRequest
+-- DROP TABLE IF EXISTS RequestAccepted
+-- Create table FriendRequest (sender_id int, send_to_id int, request_date date)
+-- Create table RequestAccepted (requester_id int, accepter_id int, accept_date date)
+-- Truncate table FriendRequest
+-- insert into FriendRequest (sender_id, send_to_id, request_date) values (1, 2, '2016/06/01')
+-- insert into FriendRequest (sender_id, send_to_id, request_date) values (1, 3, '2016/06/01')
+-- insert into FriendRequest (sender_id, send_to_id, request_date) values (1, 4, '2016/06/01')
+-- insert into FriendRequest (sender_id, send_to_id, request_date) values (2, 3, '2016/06/02')
+-- insert into FriendRequest (sender_id, send_to_id, request_date) values (3, 4, '2016/06/09')
+-- Truncate table RequestAccepted
+-- insert into RequestAccepted (requester_id, accepter_id, accept_date) values (1, 2, '2016/06/03')
+-- insert into RequestAccepted (requester_id, accepter_id, accept_date) values (1, 3, '2016/06/08')
+-- insert into RequestAccepted (requester_id, accepter_id, accept_date) values (2, 3, '2016/06/08')
+-- insert into RequestAccepted (requester_id, accepter_id, accept_date) values (3, 4, '2016/06/09')
+-- insert into RequestAccepted (requester_id, accepter_id, accept_date) values (3, 4, '2016/06/10')
+
+-- Solution 1
+-- select * from FriendRequest
+-- select * from RequestAccepted;
+
+-- Individual counts SQL
+-- select COUNT ( DISTINCT CONCAT(requester_id, accepter_id) ) from RequestAccepted 
+-- SELECT COUNT(*) from FriendRequest
+
+SELECT accept_rate = 
+CASE request_count WHEN '0' THEN '0.00'
+ELSE
+CAST
+( (CAST
+(accepted_count AS float)/CAST
+(request_count AS float)) AS decimal
+(10,2))
+END
+FROM
+    (SELECT (
+    SELECT COUNT ( DISTINCT CONCAT(requester_id, accepter_id) )
+        FROM RequestAccepted ) AS accepted_count,
+        (SELECT COUNT( DISTINCT CONCAT(sender_id, send_to_id) )
+        FROM FriendRequest ) AS request_count
+)
+result
+
+
+-- select COUNT ( DISTINCT CONCAT(requester_id, accepter_id) ) from RequestAccepted 
+-- SELECT COUNT(*) from FriendRequest
+
+-- End
