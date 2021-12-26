adb0b1dd99a24da4aeba5ab09081e46d0a7b2fa9
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-26 02:48:39 -0500

    Time: 2058 ms (46.98%), Space: 0B (100.00%) - LeetHub

---
+++ students-with-invalid-departments/students-with-invalid-departments.sql
@@ -0,0 +1,34 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Departments
+-- DROP TABLE IF EXISTS Students
+-- Create table Departments (id int, name varchar(30))
+-- Create table Students (id int, name varchar(30), department_id int)
+-- Truncate table Departments
+-- insert into Departments (id, name) values (1, 'Electrical Engineering')
+-- insert into Departments (id, name) values (7, 'Computer Engineering')
+-- insert into Departments (id, name) values (13, 'Bussiness Administration')
+-- Truncate table Students
+-- insert into Students (id, name, department_id) values (23, 'Alice', 1)
+-- insert into Students (id, name, department_id) values (1, 'Bob', 7)
+-- insert into Students (id, name, department_id) values (5, 'Jennifer', 13)
+-- insert into Students (id, name, department_id) values (2, 'John', 14)
+-- insert into Students (id, name, department_id) values (4, 'Jasmine', 77)
+-- insert into Students (id, name, department_id) values (3, 'Steve', 74)
+-- insert into Students (id, name, department_id) values (6, 'Luis', 1)
+-- insert into Students (id, name, department_id) values (8, 'Jonathan', 7)
+-- insert into Students (id, name, department_id) values (7, 'Daiana', 33)
+-- insert into Students (id, name, department_id) values (11, 'Madelynn', 1)
+
+-- SOLUTION 1
+
+
+-- Select * from Departments
+-- Select * from Students
+
+
+select DISTINCT (s.id), s.name
+from Students s LEFT JOIN Departments d ON s.department_id = d.id
+where d.id is null
+ORDER BY s.id
+
+-- END
