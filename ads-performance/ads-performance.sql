e326508d95c6484357d23674250940bc3941e9e1
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-26 01:08:50 -0500

    Time: 1579 ms (52.58%), Space: 0B (100.00%) - LeetHub

---
+++ ads-performance/ads-performance.sql
@@ -0,0 +1,41 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Ads
+-- Create table Ads (ad_id int, user_id int, action varchar(20))
+-- Truncate table Ads
+-- insert into Ads (ad_id, user_id, action) values (1, 1, 'Clicked')
+-- insert into Ads (ad_id, user_id, action) values (2, 2, 'Clicked')
+-- insert into Ads (ad_id, user_id, action) values (3, 3, 'Viewed')
+-- insert into Ads (ad_id, user_id, action) values (5, 5, 'Ignored')
+-- insert into Ads (ad_id, user_id, action) values (1, 7, 'Ignored')
+-- insert into Ads (ad_id, user_id, action) values (2, 7, 'Viewed')
+-- insert into Ads (ad_id, user_id, action) values (3, 5, 'Clicked')
+-- insert into Ads (ad_id, user_id, action) values (1, 4, 'Viewed')
+-- insert into Ads (ad_id, user_id, action) values (2, 11, 'Viewed')
+-- insert into Ads (ad_id, user_id, action) values (1, 2, 'Clicked')
+
+-- SOLUTION 1
+select ad_id,
+    ctr =
+CASE  WHEN (SELECT COUNT(action)
+    from Ads
+    where ad_id= a.ad_id and action='Clicked') = 0 and (SELECT COUNT(action)
+    from Ads
+    where ad_id= a.ad_id and action='Viewed') = 0 THEN 0
+ELSE
+CAST((ROUND((SELECT COUNT(action)
+    from Ads
+    where ad_id= a.ad_id and action='Clicked') * 100.00 / ((SELECT COUNT(action)
+    from Ads
+    where ad_id= a.ad_id and action='Clicked') + 
+(SELECT COUNT(action)
+    from Ads
+    where ad_id= a.ad_id and action='Viewed')), 2)) AS DECIMAL(5,2))  END
+from Ads a
+GROUP BY ad_id
+ORDER BY ctr desc, ad_id asc
+
+
+
+
+
+-- END
