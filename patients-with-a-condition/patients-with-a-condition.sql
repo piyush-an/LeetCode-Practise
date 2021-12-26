e3d97adc08378795bfc9ccfa663683f420eca2ff
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-26 15:22:53 -0500

    Time: 700 ms (86.67%), Space: 0B (100.00%) - LeetHub

---
+++ patients-with-a-condition/patients-with-a-condition.sql
@@ -0,0 +1,18 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Patients
+-- Create table Patients (patient_id int, patient_name varchar(30), conditions varchar(100))
+-- Truncate table Patients
+-- insert into Patients (patient_id, patient_name, conditions) values (1, 'Daniel', 'YFEV COUGH')
+-- insert into Patients (patient_id, patient_name, conditions) values (2, 'Alice', '')
+-- insert into Patients (patient_id, patient_name, conditions) values (3, 'Bob', 'DIAB100 MYOP')
+-- insert into Patients (patient_id, patient_name, conditions) values (4, 'George', 'ACNE DIAB100')
+-- insert into Patients (patient_id, patient_name, conditions) values (5, 'Alain', 'DIAB201')
+
+-- SOLUTION 1 
+select *
+from Patients
+where conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%'
+
+
+ 
+-- END
