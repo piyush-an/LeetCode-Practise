893efd88de0c78123948520b7360f9213bed8cb4
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-27 04:46:45 -0500

    Time: 1966 ms (75.71%), Space: 0B (100.00%) - LeetHub

--- fix-names-in-a-table/fix-names-in-a-table.sql
+++ fix-names-in-a-table/fix-names-in-a-table.sql
@@ -5,12 +5,21 @@
 -- insert into Users (user_id, name) values (1, 'aLice')
 -- insert into Users (user_id, name) values (2, 'bOB')
 
--- SOLUTION 1
+-- SOLUTION 1 - LEN to INT not necessary
+-- select user_id, UPPER(LEFT([name],1)) + LOWER(RIGHT([name],(CAST((LEN(name)) AS INT) - 1 ))) as name
+-- from Users
+-- ORDER BY user_id
 
 
-select user_id, UPPER(LEFT([name],1)) + LOWER(RIGHT([name],(CAST((LEN(name)) AS INT) - 1 ))) as name
+-- SOLUTION 2 - Using + for concatenation
+-- select user_id, UPPER(LEFT(name,1)) +  LOWER(RIGHT(name, LEN(name)-1)) as name
+-- from Users
+-- ORDER BY user_id
+
+
+-- SOLUTION 3 - CONCAT function for concatenation
+select user_id, CONCAT(UPPER(LEFT(name,1)), LOWER(RIGHT(name, LEN(name)-1))) as name
 from Users
 ORDER BY user_id
 
-
--- 'end'
+-- END
