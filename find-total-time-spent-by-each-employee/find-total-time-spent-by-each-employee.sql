b7c24f068c2d3e9c40f97df16881b00b1a628c8d
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-27 07:06:03 -0500

    Time: 1136 ms (40.70%), Space: 0B (100.00%) - LeetHub

---
+++ find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,22 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Employees
+-- Create table Employees(emp_id int, event_day date, in_time int, out_time int)
+-- Truncate table Employees
+-- insert into Employees (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 4, 32)
+-- insert into Employees (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 55, 200)
+-- insert into Employees (emp_id, event_day, in_time, out_time) values (1, '2020-12-3', 1, 42)
+-- insert into Employees (emp_id, event_day, in_time, out_time) values (2, '2020-11-28', 3, 33)
+-- insert into Employees (emp_id, event_day, in_time, out_time) values (2, '2020-12-9', 47, 74)
+
+
+-- SOLUTION 1 
+
+SELECT
+    event_day as day, emp_id , sum (out_time - in_time) as total_time
+-- *, out_time - in_time as elapsed 
+FROM Employees
+GROUP BY event_day, emp_id 
+
+
+
+-- END
