a92e160d2da5b2bcf1fb3b46134bd53c3fd1ca9d
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-28 01:10:12 -0500

    Time: 2751 ms (5.75%), Space: 0B (100.00%) - LeetHub

---
+++ the-number-of-rich-customers/the-number-of-rich-customers.sql
@@ -0,0 +1,21 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Store
+-- Create table Store (bill_id int, customer_id int, amount int)
+-- Truncate table Store
+-- insert into Store (bill_id, customer_id, amount) values (6, 1, 549)
+-- insert into Store (bill_id, customer_id, amount) values (8, 1, 834)
+-- insert into Store (bill_id, customer_id, amount) values (4, 2, 394)
+-- insert into Store (bill_id, customer_id, amount) values (11, 3, 657)
+-- insert into Store (bill_id, customer_id, amount) values (13, 3, 257)
+
+-- SOLUTION 1 
+
+SELECT  
+-- customer_id
+COUNT(DISTINCT(customer_id)) as rich_count
+FROM Store
+where amount > 500
+
+
+
+-- END, 
