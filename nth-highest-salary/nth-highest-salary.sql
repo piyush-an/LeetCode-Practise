eb19f75c9c32e87ea236e491e05c75a025c35869
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-30 08:27:02 -0500

    Time: 977 ms (65.39%), Space: 0B (100.00%) - LeetHub

--- nth-highest-salary/nth-highest-salary.sql
+++ nth-highest-salary/nth-highest-salary.sql
@@ -5,8 +5,20 @@ BEGIN
     
     RETURN (
         /* Write your T-SQL query statement below. */
-        select TOP (1) salary from (
-SELECT DISTINCT TOP(@N) salary from Employee ORDER BY salary desc) result
-ORDER BY salary 
+        -- SOLUTION 1
+        -- select TOP (1) salary from (
+        -- SELECT DISTINCT TOP(@N) salary from Employee ORDER BY salary desc) result
+        -- ORDER BY salary 
+        
+        -- SOLUTION 2
+        select TOP (1)
+            salary
+        from
+            (SELECT *, DENSE_RANK() OVER (ORDER BY salary desc) as dense_ranking
+            from Employee
+            ) resultset2
+        where dense_ranking = @N
+        ORDER BY salary
+        
     );
 END
