7678324b493255c0546f75b2ef83e15c5a5d49e2
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-30 11:27:25 -0500

    Time: 1062 ms (64.95%), Space: 0B (100.00%) - LeetHub

--- department-highest-salary/department-highest-salary.sql
+++ department-highest-salary/department-highest-salary.sql
@@ -16,12 +16,28 @@
 
 -- SOLUTION 1
 
-SELECT d.name as Department, rs.name as Employee, rs.salary as Salary
-from (
-SELECT name, departmentId, salary,
-        DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary desc) as ranking
-    FROM Employee )rs JOIN Department d ON rs.departmentId = d.id
-where ranking = 1
+-- SELECT d.name as Department, rs.name as Employee, rs.salary as Salary
+-- from (
+-- SELECT name, departmentId, salary,
+--         DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary desc) as ranking
+--     FROM Employee )rs JOIN Department d ON rs.departmentId = d.id
+-- where ranking = 1
+
+
+-- SOLUTION 2
+with
+    find_max
+    as
+    (
+        select DepartmentId, MAX (salary) as max_salary
+        from Employee
+        group by departmentId
+    )
+
+Select d.name as Department,
+    e.name as Employee,
+    e.salary as Salary
+from find_max f JOIN Employee e ON f.DepartmentId = e.departmentId and f.max_salary = e.salary JOIN Department d ON e.DepartmentId = d.id
 
 
 -- END
