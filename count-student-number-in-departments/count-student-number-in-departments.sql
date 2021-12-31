cc84696f46ebd16cb1df8e204275a82e2394d789
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2021-12-31 09:56:00 -0500

    Time: 1037 ms (73.31%), Space: 0B (100.00%) - LeetHub

---
+++ count-student-number-in-departments/count-student-number-in-departments.sql
@@ -0,0 +1,26 @@
+-- SCHEMA
+-- DROP TABLE IF EXISTS Student
+-- DROP TABLE IF EXISTS Department
+-- Create table Student (student_id int,student_name varchar(45), gender varchar(6), dept_id int)
+-- Create table Department (dept_id int, dept_name varchar(255))
+-- Truncate table Student
+-- insert into Student (student_id, student_name, gender, dept_id) values ('1', 'Jack', 'M', '1')
+-- insert into Student (student_id, student_name, gender, dept_id) values ('2', 'Jane', 'F', '1')
+-- insert into Student (student_id, student_name, gender, dept_id) values ('3', 'Mark', 'M', '2')
+-- Truncate table Department
+-- insert into Department (dept_id, dept_name) values ('1', 'Engineering')
+-- insert into Department (dept_id, dept_name) values ('2', 'Science')
+-- insert into Department (dept_id, dept_name) values ('3', 'Law')
+
+-- SOLUTION 1
+
+with find_people as (
+SELECT dept_id, COUNT(dept_id) OVER (PARTITION BY dept_id) as student_number FROM  Student )
+
+select dept_name, ISNULL(student_number, 0) as student_number
+from find_people fp RIGHT JOIN Department d ON fp.dept_id = d.dept_id
+group by dept_name, student_number
+order by student_number desc, dept_name
+
+
+-- END
