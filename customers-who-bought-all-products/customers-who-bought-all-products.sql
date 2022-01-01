d9fdd0718efccf707f3db9bb5c612616db618190
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2022-01-01 08:10:45 -0500

    Time: 2965 ms (56.62%), Space: 0B (100.00%) - LeetHub

---
+++ customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,28 @@
+-- SCHEMA
+-- DROP TABLE Product
+-- DROP TABLE Customer
+-- Create table Customer (customer_id int, product_key int)
+-- Create table Product (product_key int)
+-- Truncate table Customer
+-- insert into Customer (customer_id, product_key) values ('1', '5')
+-- insert into Customer (customer_id, product_key) values ('2', '6')
+-- insert into Customer (customer_id, product_key) values ('3', '5')
+-- insert into Customer (customer_id, product_key) values ('3', '6')
+-- insert into Customer (customer_id, product_key) values ('1', '6')
+-- insert into Customer (customer_id, product_key) values ('1', '6')
+-- Truncate table Product
+-- insert into Product (product_key) values ('5')
+-- insert into Product (product_key) values ('6')
+
+-- SOLUTION 1
+
+-- SELECT * FROM Product
+-- SELECT * FROM Customer
+
+-- SELECT *, COUNT(product_key) OVER (PARTITION BY customer_id) as buy_count FROM Customer
+
+SELECT DISTINCT (customer_id) FROM (
+SELECT *, COUNT(product_key) OVER (PARTITION BY customer_id) as buy_count FROM (
+select DISTINCT customer_id, product_key from Customer ) result_set ) result_set_two where buy_count = (SELECT COUNT(product_key) from Product )
+
+-- END
