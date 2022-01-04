93505634dc27c60dccfe66dbf2d6c81d8e5c822a
Piyush Anand <91434017+piyush-an@users.noreply.github.com>
2022-01-04 04:42:05 -0500

    Time: 802 ms (61.88%), Space: 0B (100.00%) - LeetHub

--- 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
+++ 1270-all-people-report-to-the-given-manager/1270-all-people-report-to-the-given-manager.sql
@@ -13,21 +13,12 @@
 
 
 -- SOLUTION 1
--- select employee_id FROM Employees 
--- where manager_id IN
--- (select employee_id FROM Employees 
--- where manager_id IN (SELECT employee_id FROM Employees
--- where manager_id = 1)) and employee_id != 1
 
+select employee_id FROM Employees 
+where manager_id IN
+(select employee_id FROM Employees 
+where manager_id IN (SELECT employee_id FROM Employees
+where manager_id = 1)) and employee_id != 1
 
--- SOLUTION 2
-SELECT
-e1.employee_id
-FROM Employees e1
-JOIN Employees e2
-ON e1.manager_id = e2.employee_id
-JOIN Employees e3
-ON e2.manager_id = e3.employee_id
-WHERE e3.manager_id = 1 AND e1.employee_id != 1
 
 -- END
