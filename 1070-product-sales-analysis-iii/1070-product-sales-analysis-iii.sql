10e6294b7415db2e646cdfa6c535b0197c919a0f
Piyush Anand <anand.pi@northeastern.edu>
2022-04-03 18:41:55 -0400

    Time: 5639 ms (10.82%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,39 @@
+-- SCHEMA
+-- DROP TABLE Product
+-- DROP TABLE Sales
+-- Create table Sales (sale_id int, product_id int, [year] int, quantity int, price int)
+-- Create table Product (product_id int, product_name varchar(10))
+-- Truncate table Sales
+-- insert into Sales (sale_id, product_id, [year], quantity, price) values ('1', '100', '2008', '10', '5000')
+-- insert into Sales (sale_id, product_id, [year], quantity, price) values ('2', '100', '2009', '12', '5000')
+-- insert into Sales (sale_id, product_id, [year], quantity, price) values ('7', '200', '2011', '15', '9000')
+-- Truncate table Product
+-- insert into Product (product_id, product_name) values ('100', 'Nokia')
+-- insert into Product (product_id, product_name) values ('200', 'Apple')
+-- insert into Product (product_id, product_name) values ('300', 'Samsung')
+
+-- SOLUTION 2
+select product_id, [year] first_year, quantity, price from (
+select *,
+RANK() OVER (PARTITION BY product_id ORDER BY year) as rankings
+from Sales ) resultset
+where rankings = 1
+
+-- SOLUTION 1
+-- SELECT * FROM Product
+-- SELECT * FROM Sales
+
+-- with
+--     result_set
+--     as
+--     (
+--         SELECT product_id, MIN([year]) as [year]
+--         FROM Sales
+--         GROUP BY product_id
+--     )
+
+-- SELECT s1.product_id , s1.year as first_year, s1.quantity, s1.price
+-- from Sales s1 JOIN result_set rs ON  s1.product_id = rs.product_id and s1.year = rs.year
+
+
+-- END
